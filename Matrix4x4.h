4fa65152477c6379b889f0fe284f44c3a5657c53
youto023 <yuuto9036@gmail.com>
2024-10-28 14:42:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Matrix4x4.h
@@ -0,0 +1,5 @@
+#pragma once
+
+struct Matrix4x4 final {
+	float m[4][4];
+};
---
+++ Resources/shaders/Object3d.PS.hlsl
@@ -0,0 +1,29 @@
+#include"Object3d.hlsli"
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct Material
+{
+    float32_t4 color;
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+
+
+
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    output.color = gMaterial.color;
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    output.color = gMaterial.color * textureColor;
+    return output;
+}
+
+
